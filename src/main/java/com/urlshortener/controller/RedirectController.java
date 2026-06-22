3008a1ad59c46398a460ee8a4fae6fd588ed9c78
MADHURI-HS <hsmadhuri7@gmail.com>
2026-06-22 20:16:24 +0530

    changes in 2nd controller

--- src/main/java/com/urlshortener/controller/RedirectController.java
+++ src/main/java/com/urlshortener/controller/RedirectController.java
@@ -4,10 +4,11 @@ import com.urlshortener.service.UrlService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class RedirectController {
+
     private final UrlService urlService;
 
     public RedirectController(UrlService urlService) {
