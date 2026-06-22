57a10b4e1645fa62e2c4f33b17f7c331a93f5549
MADHURI-HS <hsmadhuri7@gmail.com>
2026-06-22 19:09:07 +0530

    Fix root path mapping

--- src/main/java/com/urlshortener/controller/UrlController.java
+++ src/main/java/com/urlshortener/controller/UrlController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class UrlController {
 
     private final UrlService urlService;
